d6101bd86d5fe1a5ece039a6b8cd8da084d0b7ae
David Munro <david.munro@strataoncology.com>
2021-11-13 10:20:00 -0500

    Initial refactor

---
+++ src/http/post-index/event_handler.py
@@ -0,0 +1,98 @@
+import re
+import arc.tables
+
+from random import randrange
+from event import Event
+
+
+class EventHandler:
+    def __init__(self, slack_api):
+        self.slack_api = slack_api
+
+    def valid_message(self, event: Event):
+        if not event.text:
+            return False
+
+        # only respond to messages, that also aren't from bots
+        is_message = event.type == "message"
+        is_from_human = event.subtype != "bot_message" and not event.bot_id
+        return is_message and is_from_human
+
+    def handle_message(self, event: Event):
+        if event.text == "shibboleth reload":
+            self._reload_users(event)
+            return {"statusCode": 200}
+
+        event_text_matches = self._get_event_actions(event.text)
+
+        if not event_text_matches:
+            return
+
+        for i in event_text_matches:
+            delta = 1
+            if re.findall(r"\s?\-\-$", i):
+                delta = -1
+            # hack all the things
+            i = i.replace(" ", "")
+            i = i.replace("++", "").replace("--", "")
+            # endhack
+            # look up potential users
+
+            delta *= randrange(100)
+            if self._is_slack_user_id(i):
+                users_table = arc.tables.table(tablename="users")
+                ddb_item = users_table.get_item(Key={"id": i})
+                print(f"user tables: {users_table}")
+                if "Item" in ddb_item:
+                    item = ddb_item["Item"]
+                    print(f"ddb_item[Item]: {ddb_item['Item']}")
+                    print(f"is_slack_user_id (i) - before: {i}")
+                    i = item["name"]
+                    print(f"is_slack_user_id (i) - after: {i}")
+
+            # don't allow for modification of self-karma
+            event_user = f"<@{event.user}>"
+            print(f"i: {i}")
+            print(f"event_user: {event_user}")
+            print(f"item: {item['id']}")
+            if item["id"] == event_user:
+                response_text = "{}, {}".format(
+                    "Let go of your ego" if delta > 0 else "Hang on to your ego",
+                    event_user,
+                )
+            # get and modify karma
+            else:
+                karma_table = arc.tables.table(tablename="karma")
+                ddb_item = karma_table.get_item(Key={"entity": i})
+                item = {}
+                if "Item" in ddb_item:
+                    item = ddb_item["Item"]
+                    item["karma"] += delta
+                else:
+                    item = {"entity": i, "karma": delta}
+                karma_table.put_item(Item=item)
+                response_text = "_New karma for_ *{}* `{}`".format(i, item["karma"])
+            # post to channel
+            self.slack_api.post_slack_message(event.channel, response_text)
+
+    def _get_event_actions(self, event_text):
+        event_text = event_text.replace(" ", "")
+        return [
+            re.sub('"|"|"', "", m[0])
+            for m in re.findall(r'((\S+|".*"|".*")(\+\+|--))', event_text)
+        ]
+
+    def _is_slack_user_id(self, s):
+        return re.match("<@((U|W)\w+)>", s)
+
+    def _reload_users(self, event):
+        users = self.slack_api.get_slack_users_list()
+        users_table = arc.tables.table(tablename="users")
+        for i in users:
+            if i.get("name"):
+                item = {
+                    "name": i["name"],
+                    "id": "<@{}>".format(i["id"]),
+                }
+                users_table.put_item(Item=item)
+        self.slack_api.post_slack_message(event.channel, f"Reloaded {len(users)} users")
---
+++ src/http/post-index/slack_api.py
@@ -0,0 +1,88 @@
+import json
+import hashlib
+import hmac
+import time
+
+from os import environ
+from urllib import parse, request
+
+SLACK_API_BASE_URL = "https://slack.com/api/"
+SLACK_OAUTH_ACCESS_TOKEN = environ["SLACK_OAUTH_ACCESS_TOKEN"]
+SLACK_SIGNING_SECRET = environ["SLACK_SIGNING_SECRET"]
+
+
+class SlackApi:
+    def event_needs_verification(self, body):
+        return body and "challenge" in body
+
+    def verify_event(self, body):
+        # https://api.slack.com/events/url_verification
+        return {
+            "headers": {
+                "cache-control": "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
+                "content-type": "text/json; charset=utf8",
+            },
+            "statusCode": 200,
+            "body": json.dumps({"challenge": body["challenge"]}),
+        }
+
+    def validate_slack_request(self, headers, body):
+        # https://api.slack.com/post_slack_messageauthentication/verifying-requests-from-slack#a_recipe_for_security
+        if (
+            "X-Slack-Request-Timestamp" not in headers
+            or "X-Slack-Signature" not in headers
+        ):
+            return False
+        timestamp = headers["X-Slack-Request-Timestamp"]
+        if abs(time.time() - int(timestamp)) > 60 * 5:
+            return False
+        sig_basestring = "v0:" + timestamp + ":" + body
+        req_hash = (
+            "v0="
+            + hmac.new(
+                str.encode(SLACK_SIGNING_SECRET),
+                str.encode(sig_basestring),
+                hashlib.sha256,
+            ).hexdigest()
+        )
+        if not hmac.compare_digest(req_hash, headers["X-Slack-Signature"]):
+            return False
+        return True
+
+    def get_slack_users_list(self, cursor=None):
+        users = []
+        data = {}
+        if cursor:
+            data["cursor"] = cursor
+        res = json.loads(
+            self._slack_api_call(
+                "{}{}".format(SLACK_API_BASE_URL, "users.list"), data
+            ).decode("utf-8")
+        )
+        if res.get("members"):
+            users += res["members"]
+        if res.get("response_metadata", {}).get("next_cursor", None):
+            users += self.get_slack_users_list(res["response_metadata"]["next_cursor"])
+        return users
+
+    def post_slack_message(self, channel, text):
+        data = {
+            "channel": channel,
+            "text": text,
+        }
+        self._slack_api_call(
+            "{}{}".format(SLACK_API_BASE_URL, "chat.postMessage"), data
+        )
+
+    def _slack_api_call(self, url, data):
+        # https://api.slack.com/web#basics
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        data["token"] = SLACK_OAUTH_ACCESS_TOKEN
+        data = parse.urlencode(data).encode("utf-8")
+        slack_req = request.Request(
+            url,
+            data=data,
+            method="POST",
+            headers=headers,
+        )
+        return request.urlopen(slack_req).read()
